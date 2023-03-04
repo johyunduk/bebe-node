3c5f5bb9b520834a8c25e3fd2e363b7d0f9f0d4e
johyunduk <johd321@gmail.com>
2023-03-04 15:40:34 +0900

    다이어리 설정 수정

--- dist/diary-server/common/env.d.ts
+++ dist/diary-server/common/env.d.ts
@@ -3,3 +3,5 @@ export declare const MY_NAME: any;
 export declare const JWT_SECRET_KEY: any;
 export declare const JWT_ACCESS_EXPIRES_IN: any;
 export declare const JWT_REFRESH_EXPIRES_IN: any;
+export declare const AWS_REGION: any;
+export declare const AWS_BUCKET: any;
--- src/diary-server/common/env.ts
+++ src/diary-server/common/env.ts
@@ -5,3 +5,6 @@ export const MY_NAME = commonEnv.MY_NAME
 export const JWT_SECRET_KEY = commonEnv.JWT_SECRET_KEY
 export const JWT_ACCESS_EXPIRES_IN = commonEnv.JWT_ACCESS_EXPIRES_IN
 export const JWT_REFRESH_EXPIRES_IN = commonEnv.JWT_REFRESH_EXPIRES_IN
+
+export const AWS_REGION = commonEnv.AWS_REGION
+export const AWS_BUCKET = commonEnv.AWS_BUCKET
