b3a497db81a6124e29fec06ec0074a08a244e72a
johyunduk <johd321@gmail.com>
2023-04-23 18:10:17 +0900

    mall guard 수정

--- src/diary-server/routes/default-load/mall-routes.ts
+++ src/diary-server/routes/default-load/mall-routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
 import asyncHandler from '@global-common/server/routes/helper/asyncHandler'
-import { adminGuard } from '@diary-server/routes/middleware/userGuard'
+import { adminGuard, diaryGuard } from '@diary-server/routes/middleware/userGuard'
 
 export default function mallRoutes (router = Router()) {
+  // 쇼핑몰 상품 등록
+  router.post('/mall', adminGuard, asyncHandler(postMallList))
   // 쇼핑몰 리스트 조회
-  router.get('/mall', adminGuard, asyncHandler(getMallList))
+  router.get('/mall', diaryGuard, asyncHandler(getMallList))
 
+  async function postMallList (req, res) {
+    res.json({ result: 'Success' })
+  }
   async function getMallList (req, res) {
     res.json({ result: [] })
   }
