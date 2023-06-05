12880e927b5e8d88a853dad0112aadfe99ef8a1b
johyunduk <johd321@gmail.com>
2023-06-05 22:40:36 +0900

    cors 위치 조정

--- src/global-common/server/common-server.ts
+++ src/global-common/server/common-server.ts
@@ -62,8 +62,8 @@ export default function runServer ({
     app.use(helmet())
     app.use(nocache())
     // app.use(cors(corsOptions))
-    app.use('/uploads', express.static('uploads'))
     app.use(cors())
+    app.use('/uploads', express.static('uploads'))
     app.use(requestIp.mw())
 
     app.use(main())
