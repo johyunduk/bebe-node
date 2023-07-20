13cdeae85b0d4e93541e84fd3369f2925f7ce522
johyunduk <johd321@gmail.com>
2023-07-20 12:22:19 +0900

    쇼핑몰 리스트 인증 제거

--- src/diary-server/routes/default-load/mall-routes.ts
+++ src/diary-server/routes/default-load/mall-routes.ts
@@ -23,7 +23,7 @@ export default function mallRoutes (router = Router()) {
   // 쇼핑몰 상품 등록
   router.post('/mall/item', diaryGuard, localUpload.single('file'), asyncHandler(postMallItem))
   // 쇼핑몰 리스트 조회
-  router.get('/mall/item', diaryGuard, asyncHandler(getMallList))
+  router.get('/mall/item', asyncHandler(getMallList))
   // 쇼핑몰 상품 상세
   router.get('/mall/item/:id', diaryGuard, asyncHandler(getMallDetail))
   // 쇼핑몰 상품 수정
